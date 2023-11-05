57f2e350248281c44ec021b322e8aea3a8a650ea
AlexKosha <ovelikoshich@gmail.com>
2023-11-05 14:29:59 +0100

    feedBack

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,59 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import Section from './FeedBack/Section';
+import FeedbackOptions from './FeedBack/FeedbackOptions ';
+import Statistics from './FeedBack/Statistics';
+import Notification from './FeedBack/Notification';
+
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  onLeaveFeedback = key => {
+    this.setState(prevSate => ({
+      [key]: prevSate[key] + 1,
+    }));
+  };
+
+  countTotalFeedBack = () => {
+    return this.state.good + this.state.neutral + this.state.bad;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    return (this.state.good / this.countTotalFeedBack()) * 100;
+  };
+  render() {
+    const { good, neutral, bad } = this.state;
+    const total = this.countTotalFeedBack();
+    const positivePercentage =
+      this.countPositiveFeedbackPercentage().toFixed(1);
+    return (
+      <div>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={Object.keys(this.state)}
+            onLeaveFeedback={this.onLeaveFeedback}
+          />
+        </Section>
+
+        <Section title="Statistics">
+          {total === 0 ? (
+            <Notification message="There is no feedback" />
+          ) : (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={total}
+              positivePercentage={positivePercentage}
+            />
+          )}
+        </Section>
+      </div>
+    );
+  }
+}
+
+export default App;
